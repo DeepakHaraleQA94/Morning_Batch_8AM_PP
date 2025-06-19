2d1370d5fcfa81f1b07fc250b09f9977e51d73e2
DeepakHaraleQA94 <haraledeepak6@gmail.com>
2025-06-19 08:29:29 +0530

    i have done automation script

---
+++ src/main/java/ComputerDepartment/ClassA.java
@@ -0,0 +1,14 @@
+package ComputerDepartment;
+
+public class ClassA {
+
+	
+	static String name ="kiran";
+	static int RollNo=12;
+	
+	public static void main(String[] args) {
+		
+		System.out.println(name);
+		System.out.println(RollNo);
+	}
+}
